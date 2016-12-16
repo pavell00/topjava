4fc58c76fe43d89d2b06797c697eae76a3d524b3
pavell00 <mymmo88@gmail.com>
2016-12-16 10:36:25 +0300

    master_lesson2_4

---
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.repository;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public interface MealRepository {
+    Meal save(Meal meal);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    Meal get(int id);
+
+    // null if not found
+    //User getByEmail(String email);
+
+    List<Meal> getAll();
+}
