70eaef036ded3f28ef88b598498ce25b2750b8df
pavell00 <mymmo88@gmail.com>
2016-12-30 15:15:22 +0300

    lesson3 HW3 with all patch w/o optional - ok

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -0,0 +1,86 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * User: gkislin
+ * Date: 26.08.2014
+ */
+
+@Repository
+public class JdbcMealRepositoryImpl implements MealRepository {
+
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    public JdbcMealRepositoryImpl(DataSource dataSource) {
+        this.insertMeal = new SimpleJdbcInsert(dataSource)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+    }
+    @Override
+    public Meal save(Meal meal, int userId) {
+
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("user_id", userId)
+                .addValue("date_time", meal.getDateTime())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories());
+        if (meal.isNew()) {
+            Number newKey = insertMeal.executeAndReturnKey(map);
+            meal.setId(newKey.intValue());
+        } else {
+            namedParameterJdbcTemplate.update(
+                    "UPDATE meals SET date_time=:date_time, description=:description, calories=:calories " +
+                            " WHERE id=:id and user_id=:user_id", map);
+        }
+        return meal;
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return jdbcTemplate.update("DELETE FROM meals WHERE id=? and user_id=?", id, userId) != 0;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals WHERE id=? and user_id=?",
+                ROW_MAPPER, id, userId);
+        return DataAccessUtils.singleResult(meals);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return jdbcTemplate.query("SELECT * FROM meals ORDER BY date_time DESC", ROW_MAPPER);
+    }
+
+    @Override
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals " +
+                        "WHERE date_time BETWEEN ? and ? and user_id=?",
+                ROW_MAPPER, startDate, endDate,  userId);
+        return meals;
+    }
+}
---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,24 @@
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password');
+
+INSERT INTO users (name, email, password)
+VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+  (100000,	'2016-05-30 10:00:00',	'Завтрак',	500),
+  (100000,	'2016-05-30 13:00:00',	'Обед',	1000),
+  (100000,	'2016-05-30 20:00:00',	'Ужин',	500),
+  (100000,	'2016-05-31 10:00:00',	'Завтрак', 500),
+  (100000,	'2016-05-31 13:00:00',	'Обед',	1000),
+  (100000,	'2016-05-31 20:00:00',	'Ужин',	510),
+  (100001,	'2016-06-01 14:00:00',	'Админ ланч'	,510),
+  (100001,	'2016-06-01 21:00:00',	'Админ ужин',	1500)
+
