c707363d877bb17cb29d7491a4b10c796b775f84
user <mymmo88@gmai;.com>
2017-01-03 18:12:34 +0300

    lesson2_hw

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -9,11 +9,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.DbPopulator;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.time.Month;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
+import static java.time.LocalDateTime.of;
 import static org.junit.Assert.*;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -41,12 +44,16 @@ public class MealServiceTest {
 
     @Test
     public void save() throws Exception {
-
+        Meal newmeal = new Meal(100002, of(2016, Month.MAY, 30, 10, 0), "Завтрак", 500);
+        Meal created = service.save(newmeal, 100000);
+        newmeal.setId(created.getId());
+        MATCHER.assertEquals(newmeal, service.get(100002, 100000));
     }
 
     @Test
     public void get() throws Exception {
-
+        Meal meal = service.get(100003,100000);
+        MATCHER.assertEquals(MEAL2, meal);
     }
 
     @Test
@@ -73,6 +80,32 @@ public class MealServiceTest {
 
     @Test
     public void update() throws Exception {
+        Meal updatedmeal = new Meal(100002, of(2016, Month.MAY, 30, 10, 0), "Завтрак", 500);
+        updatedmeal.setDescription("UpdatedEait");
+        updatedmeal.setCalories(330);
+        service.update(updatedmeal, 100000);
+        MATCHER.assertEquals(updatedmeal, service.get(100002, 100000));
+    }
 
+    @Test(expected = NotFoundException.class)
+    public void get_enemy_meal() throws Exception{
+        Meal meal = service.get(100002,100001);
+        MATCHER.assertEquals(MEAL1, meal);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void delete_enemy_meal() throws Exception{
+        Meal meal = service.get(100002,100001);
+        service.delete(meal.getId(), 100001);
+        MATCHER.assertEquals(MEAL1, meal);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void update_enemy_meal() throws Exception {
+        Meal updatedmeal = new Meal(100002, of(2016, Month.MAY, 30, 10, 0), "Завтрак", 500);
+        updatedmeal.setDescription("UpdatedEait");
+        updatedmeal.setCalories(330);
+        service.update(updatedmeal, 100001);
+        MATCHER.assertEquals(updatedmeal, service.get(100002, 100001));
     }
 }
